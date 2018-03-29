51284bdbd6972564b3318b34145667b4a0bf99b8
Pedro Silva <p21silva98@outlook.com>
2018-03-29 19:54:31 +0100

    [dkeep.test] Hero moves into closed door
    Check symbol of door before and after movement, it should remain the
    same(closed)

--- src/dkeep/test/TestKeepGameLogic.java
+++ src/dkeep/test/TestKeepGameLogic.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import dkeep.logic.Cmd;
+import dkeep.logic.Coord;
 import dkeep.logic.Game;
 import dkeep.logic.Ogre;
 import dkeep.logic.Symbol;
@@ -14,8 +15,8 @@ public class TestKeepGameLogic {
 	char[][] map = {
 			{'X','X','X','X','X'},
 			{'X','H',' ','o','X'},
-			{'X',' ',' ',' ','X'},
-			{'i','k',' ',' ','X'},
+			{'i',' ',' ',' ','X'},
+			{'X','k',' ',' ','X'},
 			{'X','X','X','X','X'}
 			
 	};
@@ -25,7 +26,7 @@ public class TestKeepGameLogic {
 		Game game = new Game(map);
 		Ogre o = (Ogre)game.getLevel().getEnemies().get(0);
 		assertEquals(false,o.getIsStun());
-		game.moveHero(Cmd.LEFT);
+		game.moveHero(Cmd.RIGHT);
 		assertEquals(true,o.getIsStun());
 	}
 
@@ -37,4 +38,16 @@ public class TestKeepGameLogic {
 		game.moveHero(Cmd.DOWN);
 		assertEquals(Symbol.HERO_WITH_KEY,game.getLevel().getHero().getSymb());
 	}
+	
+	@Test
+	public void heroMovesIntoClosedKeepDoorNoKey() {
+		Game game = new Game(map);
+		game.moveHero(Cmd.DOWN);
+		Coord coord = new Coord(2,0);
+		assertEquals(Symbol.DOOR_CLOSED, game.getLevel().getMap().getBotEnt(coord).getSymb());
+		game.moveHero(Cmd.LEFT);
+		assertEquals(Symbol.DOOR_CLOSED, game.getLevel().getMap().getBotEnt(coord).getSymb());
+	}
+	
+
 }
