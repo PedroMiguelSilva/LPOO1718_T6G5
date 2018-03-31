549faba42029bc26e7f4b405a907d379baa5fa16
Pedro Silva <p21silva98@outlook.com>
2018-03-31 21:18:16 +0100

    [Documentation] Interactive.java

--- src/dkeep/logic/Interactive.java
+++ src/dkeep/logic/Interactive.java
@@ -2,17 +2,34 @@ package dkeep.logic;
 
 import java.util.ArrayList;
 
+/* Nominates an Interactive Entity
+ * @version 1.0
+ * @since 1.0
+ */
 abstract public class Interactive extends Entity
 {
-	//Attributes
-	
-	//Constructor
+	/* Create an Interactive object with specified position and symbol
+	 * @param startX
+	 * 			The Interactive's x position
+	 * @param startY
+	 * 			The Interactive's y position
+	 * @param startSymbol
+	 * 			The Interactive's start symbol
+	 */
 	public Interactive(int startX, int startY, Symbol startSymb)
 	{
 		super(startX,startY,startSymb);
 	}
 	
-	//Methods
+	/* Hero moves towards the Door
+	 * Hero opens the Door with Key if it's closed, moves Hero on top of Door if it's open
+	 * @param hero
+	 * 			Hero that is moving against the Door
+	 * @param interactives
+	 * 			Array of Interactive objects that might be triggered
+	 * @param map
+	 * 			Map in which the updates should be made
+	 */
 	abstract public void trigger(Hero hero, ArrayList<Interactive> interactives,Map map);
 
 }
