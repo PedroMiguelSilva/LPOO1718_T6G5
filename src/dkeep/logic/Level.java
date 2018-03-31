7c394896e36d8aea7e6389058a8e65473926db5c
Pedro Silva <p21silva98@outlook.com>
2018-03-31 21:36:53 +0100

    [Documentation] Level.java

--- src/dkeep/logic/Level.java
+++ src/dkeep/logic/Level.java
@@ -2,16 +2,25 @@ package dkeep.logic;
 
 import java.util.ArrayList;
 
+/* Nomiantes a Level
+ * @version 1.0
+ * @since 1.0
+ */
 abstract public class Level
 {
-	//Attributes
 	private Hero hero;
 	private ArrayList<Enemy> enemies;
 	private ArrayList<Interactive> interactives;
 	private Map map;
 	
+	/* Checks if the specific level winning condition has been reached
+	 * @return Winning condition was met or not
+	 */
 	abstract public boolean hasWonLevel();
 	
+	/* Check if the hero dies from current Map
+	 * @return Hero is dead
+	 */
 	public boolean heroDied() {
 		if(this.getHero().isDead(this.getMap(), this.getEnemies())) {
 			this.getHero().setDead();
@@ -20,6 +29,10 @@ abstract public class Level
 		return false;
 	}
 
+	/* Move Enemies of the level
+	 * @param enemies
+	 * 			Enemies of the level
+	 */
 	public void moveEnemies(ArrayList<Enemy> enemies) {
 		for(Enemy e: this.getEnemies())
 		{
@@ -27,10 +40,9 @@ abstract public class Level
 		}
 	}
 	
-	/*
-	 * @brief 	updates the logic of the game acording to heroMovement
-	 * @param 	heroMovement
-	 * 				direction in which the user wants to move
+	/* Update the logic of the game according to command from User
+	 * @param 	cmd
+	 * 				Direction in which the user wants to move
 	 * @return	0
 	 * 				keep playing
 	 * 			1
@@ -39,8 +51,6 @@ abstract public class Level
 	 * 				level up
 	 */
 	public int update(Cmd cmd) {
-		
-		//move hero		
 		this.getHero().move(getMap(), cmd, getInteractives(), getEnemies());
 		
 		//check if won
@@ -64,9 +74,11 @@ abstract public class Level
 		return 0;
 	}
 
-	
-	//Methods
-	
+	/* Create Walls for a Map
+	 * @param coord
+	 * 			Coordinates of the walls to be created
+	 * @return Array of the walls created
+	 */
 	public ArrayList<Entity> createWalls(ArrayList<Coord> coord){
 		ArrayList<Entity> result = new ArrayList<Entity>();
 	
@@ -78,6 +90,11 @@ abstract public class Level
 		return result;
 	}
 	
+	/* Create Doors for a Map
+	 * @param coord
+	 * 			Coordinates of the doors to be created
+	 * @return Array of Doors created
+	 */
 	public ArrayList<Entity> createDoors(ArrayList<Coord> coord){
 		ArrayList<Entity> result = new ArrayList<Entity>();
 		
@@ -88,6 +105,14 @@ abstract public class Level
 		return result;
 	}
 	
+	/* Transform two arrays of x and y positions and converts into an array of coordinates
+	 * @Note : A coordinate is created from values of xPos and yPos of same index
+	 * @param posX
+	 * 			Array of x values
+	 * @param posY
+	 * 			Array of y values
+	 * @return Array of Coordinates created from array of x and y values
+	 */
 	public ArrayList<Coord> posToCoords(int[] posX, int[] posY){
 		ArrayList<Coord> coords = new ArrayList<Coord>();
 		
@@ -99,30 +124,37 @@ abstract public class Level
 		return coords;
 	}
 	
-	
-	
-	
-	//GET METHODS
+	/* @return Map of the Level
+	 */
 	public Map getMap()
 	{
 		return this.map;
 	}
 
+	/* @return Hero of the Level
+	 */
 	public Hero getHero()
 	{
 		return this.hero;
 	}
 
+	/* @return Enemies of the Level
+	 */
 	public ArrayList<Enemy> getEnemies()
 	{
 		return this.enemies;
 	}
 
+	/* @return Interactive objects of the Level
+	 */
 	public ArrayList<Interactive> getInteractives()
 	{
 		return this.interactives;
 	}
 
+	/* Search the Map for the Hero and returns it
+	 * @return Hero of the Level
+	 */
 	public Hero getHeroFromMap() {
 		for(int i = 0 ; i < map.getMap().length; i++) {
 			for(int j = 0; j < map.getMap()[0].length; j++) {
@@ -134,6 +166,9 @@ abstract public class Level
 		return null;
 	}
 	
+	/* Search the Map for the Enemies and returns them
+	 * @return Array of Enemies of the Level
+	 */
 	public ArrayList<Enemy> getEnemiesFromMap(){
 		ArrayList<Enemy> result = new ArrayList<Enemy>();
 		for(int i = 0 ; i < map.getMap().length; i++) {
@@ -146,6 +181,9 @@ abstract public class Level
 		return result;
 	}
 	
+	/* Search the Map for the Interactive objects and returns them
+	 * @return Array of Interactive objects of the Level
+	 */
 	public ArrayList<Interactive> getInteractivesFromMap(){
 		ArrayList<Interactive> result = new ArrayList<Interactive>();
 		
@@ -162,22 +200,37 @@ abstract public class Level
 		return result;
 	}
 	
-	//SET METHODS
+	/* Set new Map to the Level
+	 * @param newMap
+	 * 			New Map
+	 */
 	public void setMap(Map newMap)
 	{
 		this.map = newMap;
 	}
 	
+	/* Set new Enemies to the Level
+	 * @param newEnemies
+	 * 			New Enemies
+	 */
 	public void setEnemies(ArrayList<Enemy> newEnemies)
 	{
 		this.enemies = newEnemies;
 	}
 
+	/* Set new Interactive objects to the Level
+	 * @param newInteractives
+	 * 			New Interactive objects
+	 */
 	public void setInteractives(ArrayList<Interactive> newInteractives)
 	{
 		this.interactives = newInteractives;
 	}
 
+	/* Set new Hero to the Level
+	 * @param newHero
+	 * 			New Hero
+	 */
 	public void setHero(Hero newHero)
 	{
 		this.hero = newHero;
