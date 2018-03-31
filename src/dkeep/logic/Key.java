5a9d378ae7aa6ffe45502999e29682bde00396cd
Pedro Silva <p21silva98@outlook.com>
2018-03-31 21:24:48 +0100

    [Documentation] Key.java

--- src/dkeep/logic/Key.java
+++ src/dkeep/logic/Key.java
@@ -2,23 +2,50 @@ package dkeep.logic;
 
 import java.util.ArrayList;
 
+/* Represents a Key
+ * @Note : A Key with doorC coordinates of x = 0 and y = 0 is universal and will open any door
+ * @version 1.0
+ * @since 1.0
+ */
 public class Key extends Interactive
 {
-	//Attributes
 	Coord doorC;
 
-	//Constructor
-	public Key(int startX, int startY, int x, int y)
+	/* Creates a key with specified start position and coordinates of the door it opens
+	 * @param startX
+	 * 			The Key's x positon
+	 * @param startY
+	 * 			The Key's y position
+	 * @param doorX
+	 * 			The x coordinate of the door opened by this Key
+	 * @param doorY
+	 * 			The y coordinate of the door opened by this Key
+	 */
+	public Key(int startX, int startY, int doorX, int doorY)
 	{
 		super(startX,startY,Symbol.KEY);
-		doorC = new Coord(x,y);
+		doorC = new Coord(doorX,doorY);
 	}
 
+	/* Creates an universal Key with specified position
+	 * @param startX
+	 * 			The Key's x position
+	 * @param startY
+	 * 			The Key's y position 
+	 */
 	public Key(int startX, int startY){
 		super(startX,startY,Symbol.KEY);
 		doorC = new Coord(0,0);
 	}
 	
+	/* Hero walks into the key and picks it up
+	 * @param hero
+	 * 			Hero that picks the Key
+	 * @param interactives
+	 * 			Array of interactive objects that might be interacted with
+	 * @param map
+	 * 			Map in which the changes should be made
+	 */
 	@Override
 	public void trigger(Hero hero, ArrayList<Interactive> interactives, Map map)
 	{
@@ -28,10 +55,10 @@ public class Key extends Interactive
 		map.move(hero, this.getCoord());
 		hero.getCoord().setCoord(this.getCoord());
 		map.setBotEnt(hero.getCoord(), temp);
-	}
+	}	
 
-	//Methods
-
+	/* @return Coordinates of the door opened by this Key
+	 */
 	public Coord getDoorCoord() {
 		return this.doorC;
 	}
