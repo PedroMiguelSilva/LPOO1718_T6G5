0283fd85acecde625f15ab18c5fb2892af5f184d
Pedro Silva <p21silva98@outlook.com>
2018-03-31 16:50:07 +0100

    [Documentation] Cmd.java

--- src/dkeep/logic/Cmd.java
+++ src/dkeep/logic/Cmd.java
@@ -1,13 +1,37 @@
 package dkeep.logic;
 
 /*
- * USED TO SAVE THE COMMAND USED BY THE USER
+ * Commands that can be used by the user
+ * <li>{@link #START}</li>
+ * <li>{@link #UP}</li>
+ * <li>{@link #DOWN}</li>
+ * <li>{@link #RIGHT}</li>
+ * <li>{@link #LEFT}</li>
+ * <li>{@link #QUIT}</li>
  */
 public enum Cmd {
+	/*
+	 * Start command
+	 */
 	START,
+	/*
+	 * Up command
+	 */
 	UP,
+	/*
+	 * Down command
+	 */
 	DOWN,
+	/*
+	 * Right command
+	 */
 	RIGHT,
+	/*
+	 * Left command
+	 */
 	LEFT,
+	/*
+	 * Quit command
+	 */
 	QUIT;
 }
