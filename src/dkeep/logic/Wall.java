46e55c8d78659ba924114eb888e7403720563e72
Pedro Silva <p21silva98@outlook.com>
2018-04-02 04:09:31 +0100

    [Documentation] Wall.java

--- src/dkeep/logic/Wall.java
+++ src/dkeep/logic/Wall.java
@@ -1,11 +1,25 @@
 package dkeep.logic;
 
+/* Represents a Wall
+ * @version 1.0
+ * @since 1.0
+ */
 public class Wall extends Neutral{
 	
+	/* Creates a Wall Entity with specified x and y position
+	 * @param startX
+	 * 			The Wall x position
+	 * @param startY
+	 * 			The Wall y position
+	 */
 	public Wall(int startX, int startY) {
 		super(new Coord(startX,startY),Symbol.WALL);
 	}
 	
+	/* Creates a Wall with specified coordinates
+	 * @param coord
+	 * 			The coordinate of the Wall
+	 */
 	public Wall(Coord coord) {
 		super(coord,Symbol.WALL);
 	}
