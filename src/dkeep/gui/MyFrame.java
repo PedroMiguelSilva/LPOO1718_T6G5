05e0a829c072d6cb82d3b645de990d5e52cfcaee
Miguel Moura <miguelbarracamoura@gmail.com>
2018-04-06 00:01:01 +0100

    Updating former methods

---
+++ src/dkeep/gui/MyFrame.java
@@ -0,0 +1,19 @@
+package dkeep.gui;
+
+import java.awt.Color;
+import java.awt.Frame;
+
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	
+	public MyFrame() {
+
+		getContentPane().setBackground(Color.LIGHT_GRAY);
+		setBounds(100, 100, 800, 600);
+
+		
+	} 
+	
+	
+}
